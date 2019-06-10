7b4bfd8bfb8b37290cf635cc2d86d0e6573896b4
sagittatius <qiaoliang.email@gmail.com>
2019-06-10 12:58:50 +0800

    initial tasking

--- src/main/java/args/MySchema.java
+++ src/main/java/args/MySchema.java
@@ -5,4 +5,8 @@ public class MySchema {
     public MySchema(String flags) {
 
     }
+
+    public BoolRule getRule(String l) {
+        return null;
+    }
 }
---
+++ src/test/java/args/MySchemaTest.java
@@ -0,0 +1,18 @@
+package args;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySchemaTest {
+    @Test
+    void test_create_schema_with_bool() throws Exception{
+        MySchema schema = new MySchema("l:bool");
+        assertNotNull(schema);
+        assertNull(schema.getRule("l"));
+
+    }
+
+}
