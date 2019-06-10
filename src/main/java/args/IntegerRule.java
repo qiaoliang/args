c6078a7c44e0f3494c6993b50b3faf00b0a4c89a
sagittatius <qiaoliang.email@gmail.com>
2019-06-10 13:28:40 +0800

    initial tasking

--- src/main/java/args/IntegerRule.java
+++ src/main/java/args/IntegerRule.java
@@ -1,9 +1,19 @@
 package args;
 
 public class IntegerRule implements MyRule {
+    private Integer defaultvalue;
+    private final String defaultTag;
+
+    public IntegerRule() {
+        defaultvalue = new Integer(0);
+        defaultTag= "-d";
+    }
+    public IntegerRule(String tag) {
+        this.defaultTag= tag;
+    }
 
     @Override
     public Integer getValue(String commands) {
-        return 0;
+        return defaultvalue;
     }
 }
